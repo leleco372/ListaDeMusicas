d4f40ac1e7aa3638e9718cb78dc3e977d517b5c1
leleco372 <leandroalcantara123456@gmail.com>
2026-08-13 17:35:36 -0300

    criando listas, classes

--- Projeto_musica/musica.py
+++ Projeto_musica/musica.py
@@ -5,8 +5,10 @@ app = Flask(__name__)
 
 
 @app.route('/musicas')
-def listaMusicas():
-    return render_template('lista_musicas.html', titulo="Aprendendo do inicio")
+def listarMusicas():
+
+    lista = ["despacito", "shape of you", "bad guy", "blinding lights", "rockstar"]
+    return render_template('lista_musicas.html', titulo="Leandro aprendendo do zero", musicas= lista)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
