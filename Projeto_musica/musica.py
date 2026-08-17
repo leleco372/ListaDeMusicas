adc0e03e8ebd1d6f6df1c1134f8a50420d57b67c
leleco372 <leandroalcantara123456@gmail.com>
2026-08-16 21:02:22 -0300

    update do site
    Criação de padrão de layout, autenticação de usuário. Otimização das rotas e url. Mensagem para usuário no login e outras ações.

--- Projeto_musica/musica.py
+++ Projeto_musica/musica.py
@@ -1,6 +1,23 @@
 #não quero inportar tudo da biblioteca flask
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, request, redirect, session, flash, url_for
 #self é como o this do java, ele referencia o objeto que está chamando o método
+class Usuario:
+    def __init__(self, nome, senha, gmail):
+        self.nome=nome
+        self.senha=senha
+        self.gmail=gmail
+
+usuario01=Usuario("Leandro", "admin", "leandro@exemplo.com")
+usuario02=Usuario("João", "1234", "joao@exemplo.com")
+usuario03=Usuario("Maria", "abcd", "maria@exemplo.com")
+
+#criando um dicionário de usuários, onde a chave é o gmail e o valor é o objeto usuário
+usuarios = {
+    usuario01.gmail: usuario01,
+    usuario02.gmail: usuario02,
+    usuario03.gmail: usuario03
+}
+
 
 class Musica:
     def __init__(self, nome, artista, genero):
@@ -23,11 +40,16 @@ app.secret_key = "senhasupersecreta"
 
 @app.route('/')
 def listarMusicas():
+    if  "usuario_logado" not in session or session["usuario_logado"] == None:
+        return redirect(url_for('login'))
     return render_template('lista_musicas.html', musicas=lista, titulo="Lista de Músicas")
 
 @app.route("/cadastrar_musicas")
 def cadastrarMusica():
-    return render_template('cadastrar_musicas.html')
+    if  "usuario_logado" not in session or session["usuario_logado"] == None:
+        return redirect(url_for('login'))
+    
+    return render_template('cadastrar_musicas.html', titulo="Cadastrar Música")
 
 @app.route("/adicionar", methods=["POST",])
 def adicionarMusica():
@@ -39,28 +61,34 @@ def adicionarMusica():
     novaMusica=Musica(nome, artista, genero)
     if nome !="":
         lista.append(novaMusica)
-        return redirect('/')
+        return redirect(url_for('listarMusicas'))
     else:
-        return redirect('/cadastrar_musicas')
+        return redirect(url_for('cadastrarMusica'))
 
 @app.route("/login")
 def login():
-    return render_template('login.html')
+    return render_template('login.html', titulo="Login")
 
 @app.route("/autenticar", methods=["POST",])
 def autenticar():
-    if request.form["txtSenha"]== "admin":
-        #session é basicamende um divisor de águas, ele vai criar uma sessão para o usuário que está logando, e vai armazenar o nome do usuário na sessão. )
-        session["usuario_logado"] = request.form["txtUsuario"]
-
-        return redirect('/')
+    if request.form["txtUsuario"] in usuarios:
+        usuarioEncontrado = usuarios[request.form["txtUsuario"]]
+        if request.form["txtSenha"] == usuarioEncontrado.senha:
+    #session é basicamende um divisor de águas, ele vai criar uma sessão para o usuário que está logando, e vai armazenar o nome do usuário na sessão. )
+            session["usuario_logado"] = request.form["txtUsuario"]
+            flash(f"usuário: {usuarioEncontrado.nome} logado com sucesso!")
+            return redirect(url_for('listarMusicas'))
+        else:
+            flash("senha incorreta!")
+            return redirect(url_for('login'))
     else: 
-        return redirect('/login')
+        flash("usuário ou senha incorretos!")
+        return redirect(url_for('login'))
 
 @app.route("/sair")
 def sair():
     session["usuario_logado"] = None
-    return redirect('/')
+    return redirect(url_for('login'))
 
 
 if __name__ == '__main__':
